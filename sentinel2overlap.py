db0dd99ce235c21e16cad7de7a01538bd0eb6598
Alexander Baidulin <alexanderbaidulin@gmail.com>
2020-05-24 12:42:51 +0300

    Sentinel file now loads from Google Drive; change projection to UTM

--- sentinel2overlap.py
+++ sentinel2overlap.py
@@ -10,6 +10,7 @@ import argparse
 import geopandas as gpd
 import pandas as pd
 import numpy as np
+import math
 
 def read_filepaths():
     """Reads the filenames for input/output from console"""
@@ -45,6 +46,8 @@ def make_overlap_map(gdf):
 def drop_redundant(tiles, shape, overlap_map):
     """Returns a list of tile names that still qualify"""
     output = []
+    utm_zone = int(math.ceil((shape.centroid.x + 180) / 6))                                      # Get a numeric code for the UTM zone, based off the input_shape
+    utm_crs = "+proj=utm +zone={} +ellps=WGS84 +datum=WGS84 +units=m +no_defs".format(utm_zone)  # Add this new zone value to string
     if any(shape.within(overlap_map)):                                                           # Condition 3: Check if the input shape is completely within the overlap zone
         overlapping = tiles.loc[shape.within(tiles)]                                             #Returns first(least ID) tile that contains input shape
         output = overlapping.loc[:,'Name']                                                       #Save tile name into list
@@ -52,8 +55,8 @@ def drop_redundant(tiles, shape, overlap_map):
     else:
         for index in tiles.index:
             coverage = shape.intersection(tiles.at[index,'geometry'])
-            coverage_cartesian=coverage.to_crs("EPSG:3857")                                       # Set a copy of overlap to Cartesian system
-            if(coverage_cartesian.area[0]<= 1000.0):                                              # Get overlap area in m2, compare
+            coverage_UTM=coverage.to_crs(utm_crs)                                                 # Set a copy of overlap to UTM
+            if(coverage_UTM.area[0]<= 1000.0):                                                    # Get overlap area in m2, compare
                 continue                                                                          # Condition 1 : Tile coverage must me more than 1000 m2
             if any(coverage.within(overlap_map)):                                                 # Drop tile if they only cover the overlap
                 continue                                                                          # Condition 2 : Discard tiles that only cover within overlap
@@ -67,14 +70,29 @@ def print_tile_list(tile_list):
     for entry in tile_list:
         print(entry, end=' ')
 
+def load_sentinel(file_id, shape):
+    """Returns a GeoDataFrame of Sentinel tiles within the bounding box of the shape polygon"""
+        # # Using Google API
+        # service = Google API instance
+        # # file_id = 184xXr4eq41SdBDiOOogMy2ajSjKFNT7H
+        # file = service.files().get(fileId=file_id).execute()     # Gets a file metadata from Google drive, by file ID
+        # # file['title'] == 'sentinel2tiles.geojson'
+        # # file['webContentLink'] == "https://drive.google.com/uc?id=184xXr4eq41SdBDiOOogMy2ajSjKFNT7H&export=download" 
+        # # Get a link from that, plug into gpd.read_file()
+
+    url_link = "https://docs.google.com/u/0/uc?export=download&id={}".format(file_id)
+    print("Loading database, please wait...")
+
+    return gpd.read_file(url_link, bbox=list(shape.bounds.values[0]) )   # Get only the tiles that overlap the bounding box  
+
+
 if __name__ == "__main__":
     # Filepaths
     input_fp, output_fp = read_filepaths()                                                    
-    sentinel_fp = "sentinel2_tiles.geojson"
-
+    sentinel_file_ID = '184xXr4eq41SdBDiOOogMy2ajSjKFNT7H'
     # Begin work
     input_shape = gpd.read_file(input_fp)                                             # Read the input file
-    sentinel = gpd.read_file(sentinel_fp, bbox=list(input_shape.bounds.values[0]) )   # Get only the tiles that overlap the bounding box  
+    sentinel = load_sentinel(sentinel_file_ID, input_shape)                                # Load Sentinel tiles
     output_list = drop_redundant(sentinel, input_shape, make_overlap_map(sentinel))   # Discard the redundant tiles
     print_tile_list(output_list)
 
--- sentinel_unittest.py
+++ sentinel_unittest.py
@@ -1,12 +1,34 @@
 import unittest
+from unittest.mock import Mock
 import geopandas as gpd
 import sentinel2overlap as s2o
 
+
+
+
+
+
 class TestScript(unittest.TestCase):
-    test_data = gpd.read_file('sentinel_test.geojson')
+    test_data = gpd.read_file('test/sentinel_test.geojson')
     shape = gpd.read_file('Charkiv.geojson')
-    overlaps = gpd.read_file('overlaps_test.geojson')
+    overlaps = gpd.read_file('test/overlaps_test.geojson')
     correct_result = ['36UXA', '36UXU', '36UXV', '36UYA', '36UYB', '36UYU', '36UYV', '37UCP', '37UCQ', '37UCR', '37UDQ', '37UDR']
+
+    
+    def test_loadSentinel_API(self):
+        file_id = '184xXr4eq41SdBDiOOogMy2ajSjKFNT7H'
+        service = Mock()
+        file = service.files().get(fileId=file_id).execute()     # Gets a file metadata from Google drive, by file ID
+        # file['title'] == 'sentinel2tiles.geojson'
+        # file['webContentLink'] == "https://drive.google.com/uc?id=184xXr4eq41SdBDiOOogMy2ajSjKFNT7H&export=download" 
+        # Get a link from that, plug into gpd.read_file()
+        service.files.assert_called()
+        self.assertIsNotNone(file)
+
+    def test_loadSentinelReturnsCorrect(self):
+        result = s2o.load_sentinel('184xXr4eq41SdBDiOOogMy2ajSjKFNT7H', self.shape)
+        self.assertIsInstance(result, gpd.GeoDataFrame)
+        
 
     def test_OverlapReturnsCorrect(self):
         result = s2o.make_overlap_map(self.test_data)
@@ -26,7 +48,6 @@ class TestScript(unittest.TestCase):
     def test_ReadFilepathsReturnsTuple(self):
         result = s2o.read_filepaths()
         self.assertIsInstance(result, tuple)
-
 
 if __name__ == '__main__':
     unittest.main()
